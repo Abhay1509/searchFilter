12d705f9a12c52342e5fc1c10871ed6ca32b86f9
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-06-05 05:51:33 +0530

    searchComponent

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import MOCK_DATA from "./MOCK_DATA.json";
+import "./App.css";
+
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState<any>("");
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      ></input>
+
+      {MOCK_DATA.filter((val) => {
+        if (searchTerm == "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
